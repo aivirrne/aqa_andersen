cb7dd14df38fcfadddd2bfc4a96912704f62dea8
aivirrne <chaos.no.mai@gmail.com>
2022-09-13 19:46:20 +0200

    done

---
+++ school.lesson1/src/main/java/school/lesson1/HomeWorkApp.java
@@ -0,0 +1,49 @@
+package school.lesson1;
+
+public class HomeWorkApp {
+    public static void main(String[] args) {
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
+    }
+
+    public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
+    public static void checkSumSign() {
+        int a = 16;
+        int b = -40;
+        int c = a + b;
+        if (c >= 0) {
+            System.out.println("Сумма положительная или равна нулю");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+
+    }
+
+    public static void printColor() {
+        int value = 7;
+        if (value <= 0) {
+            System.out.println("Красный");
+        } else if (value > 0 && value <= 100) {
+            System.out.println("Желтый");
+        } else {
+            System.out.println("Зеленый");
+        }
+    }
+
+    public static void compareNumbers() {
+        int a = 10;
+        int b = 50;
+        if (a >= b) {
+            System.out.println("a >= b");
+        } else {
+            System.out.println("a < b");
+        }
+    }
+}
